fdffd149123c6616dcf2c0722d10b2b67b42c14b
Gustavo Rodriguez <gustavo.grodriguez@mercadolibre.com>
2018-01-04 11:30:38 -0300

    Changed dependencies.

--- app/server/index.js
+++ app/server/index.js
@@ -3,11 +3,14 @@ const React = require('react');
 const App = require('../shared/app');
 
 const server = express();
+const router = server.router();
+
+require('babel-register');
 
 // serve static assets from here
 server.use('../shared/assets', express.static('assets'));
 
-server.get('/', (req, res) => {
+router.get('/', (req, res) => {
   const View = props => (<App {...props} />);
   res.render(View, { text: 'React project' });
 });
