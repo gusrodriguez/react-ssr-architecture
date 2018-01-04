7f26fce298ad2836deecd840708870bf53da4793
Gustavo Rodriguez <gustavo.grodriguez@mercadolibre.com>
2018-01-04 12:25:50 -0300

    Added babel-register.

--- app/server/index.js
+++ app/server/index.js
@@ -1,3 +1,4 @@
+require('babel-register')();
 const express = require('express');
 const React = require('react');
 const App = require('../shared/app');
@@ -5,14 +6,14 @@ const App = require('../shared/app');
 const server = express();
 const router = server.router();
 
-require('babel-register');
 
 // serve static assets from here
 server.use('../shared/assets', express.static('assets'));
 
 router.get('/', (req, res) => {
-  const View = props => (<App {...props} />);
-  res.render(View, { text: 'React project' });
+  const data = { text: 'React project' };
+  const View = <App {...data} />;
+  res.render(View);
 });
 
 server.listen(8080);
